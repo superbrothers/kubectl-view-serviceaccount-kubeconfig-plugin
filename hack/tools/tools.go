032cb1e76d08da40ef065743efe759c8e09d6761
Kazuki Suda <230185+superbrothers@users.noreply.github.com>
2021-12-16 00:07:42 +0900

    Use Go 1.17 (#49)
    * Use Go 1.17
    
    * Update GoReleaser to v1.1.0
    
    * Remove using superbrothers/actions/krew-plugin-test action

--- hack/tools/tools.go
+++ hack/tools/tools.go
@@ -6,4 +6,5 @@ import (
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 	_ "github.com/goreleaser/goreleaser"
 	_ "github.com/hairyhenderson/gomplate/v3"
+	_ "sigs.k8s.io/krew/cmd/validate-krew-manifest"
 )
